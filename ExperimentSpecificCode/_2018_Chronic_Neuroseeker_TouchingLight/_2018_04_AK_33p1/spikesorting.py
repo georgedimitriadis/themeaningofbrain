483e97bad04caa97390e7e63c2e7c8f7ad1da7e7
georgedimitriadis <gdimitri@hotmail.com>
2018-06-28 11:48:10 +0100

    Some more spikesorting code

--- ExperimentSpecificCode/_2018_Chronic_Neuroseeker_TouchingLight/_2018_04_AK_33p1/spikesorting.py
+++ ExperimentSpecificCode/_2018_Chronic_Neuroseeker_TouchingLight/_2018_04_AK_33p1/spikesorting.py
@@ -1,24 +1,28 @@
 
 
 import numpy as np
+import pandas as pd
 from os.path import join
 from GUIs.Kilosort import clean_kilosort_templates as clean
 from GUIs.Kilosort import create_data_cubes as c_cubes
 from Layouts.Probes.Neuroseeker import probes_neuroseeker as ps
 from ExperimentSpecificCode._2018_Chronic_Neuroseeker_TouchingLight._2018_04_AK_33p1 import constants as const
-from spikesorting_tsne import tsne, preprocessing_kilosort_results as preproc_kilo
+from spikesorting_tsne import tsne, visualization as viz, preprocessing_kilosort_results as preproc_kilo, \
+     io_with_cpp as tsne_io
 
 
 # This is to create the correct prb file with only the AP channels in (1368). It should never be run again
-ps.create_1368channels_neuroseeker_prb(const.probe_layout_folder, const.prb_file)
+# ps.create_1368channels_neuroseeker_prb(const.probe_layout_folder, const.prb_file)
 # ----------------------------------------------------------------
 
 # FOLDERS NAMES --------------------------------------------------
-kilosort_folder = join(const.base_save_folder, const.rat_folder, const.date_folders[1],
+date = 5
+kilosort_folder = join(const.base_save_folder, const.rat_folder, const.date_folders[date],
                        'Analysis', 'Kilosort')
-binary_data_filename = join(const.base_save_folder, const.rat_folder, const.date_folders[1],
+binary_data_filename = join(const.base_save_folder, const.rat_folder, const.date_folders[date],
                             'Data', 'Amplifier_APs.bin')
-
+tsne_folder = join(const.base_save_folder, const.rat_folder, const.date_folders[date],
+                   'Analysis', 'Tsne')
 # ----------------------------------------------------------------
 
 
@@ -57,8 +61,7 @@ number_of_clean_spikes = len(clean_spikes)
 
 
 # Get a representative sample of spikes for t-sne
-tsne_folder = join(const.base_save_folder, const.rat_folder, const.date_folders[1],
-                   'Analysis', 'Tsne')
+
 
 representative_indices, small_templates, large_templates = \
     preproc_kilo.find_spike_indices_for_representative_tsne(kilosort_folder, tsne_folder, 5000, 700000)
@@ -67,12 +70,42 @@ template_features_matrix = preproc_kilo.calculate_template_features_matrix_for_t
                                                                                     spikes_used_with_original_indexing=
                                                                                     representative_indices)
 
+template_features_matrix = np.load(join(tsne_folder, 'data_to_tsne_(699861, 1155).npy'))
+
+
+# First run of t-sne
 num_dims = 2
 perplexity = 100
 theta = 0.3
-iterations = 2000
+iterations = 4000
 random_seed = 1
 verbose = 3
 
-tsne.t_sne(template_features_matrix, files_dir=tsne_folder, num_dims=num_dims, perplexity=perplexity,
-           theta=theta, iterations=iterations, random_seed=random_seed, verbose=verbose)
+tsne_results = tsne.t_sne(template_features_matrix, files_dir=tsne_folder, num_dims=num_dims, perplexity=perplexity,
+                          theta=theta, iterations=iterations, random_seed=random_seed, verbose=verbose)
+spike_info = preproc_kilo.generate_spike_info(kilosort_folder=kilosort_folder, tsne_folder=tsne_folder)
+
+
+# Run t-sne again with different parameters starting from the already calculated hd distances
+num_dims = 2
+perplexity = 100
+theta = 0.3
+iterations = 4000
+random_seed = 1
+verbose = 2
+
+tsne_results = tsne.t_sne_from_existing_distances(files_dir=tsne_folder, data_has_exageration=True, num_dims=num_dims,
+                                                  theta=theta, iterations=iterations, random_seed=random_seed,
+                                                  verbose=verbose)
+spike_info = preproc_kilo.generate_spike_info(kilosort_folder=kilosort_folder, tsne_folder=tsne_folder)
+
+
+# OR Load previously run t-sne
+tsne_results = tsne_io.load_tsne_result(files_dir=tsne_folder)
+
+# and previously generated spike_info
+spike_info = pd.read_pickle(join(tsne_folder, 'spike_info.df'))
+
+
+# Have a look
+viz.plot_tsne_of_spikes(spike_info=spike_info, legent_on=False)
